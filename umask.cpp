b6bdeba74a66fb74e574cb8b922ded5f64903ff5
MaJessica9 <jamma@ucdavis.edu>
2015-01-21 18:12:35 -0800

    Update umask.cpp

--- umask.cpp
+++ umask.cpp
@@ -3,20 +3,22 @@ void umask(Funix *funix, int argCount, const char *arguments[])
   // Assuming the command doesn't count as an argument
   int i;
   if (argCount != 1)
-    printf("usage: umask octal");
+    printf("usage: umask octal\n");
   else
   {
+    printf("%c\n", *arguments[0]);
+
     // Parse through value in *arguments
-    // if (*argument[0] != '0'||*argument[0] != '1' \
-	||*argument[0] != '2'||*argument[0] != '3'\
-	||*argument[0] != '4'||*argument[0] != '5'\
-	||*argument[0] != '6'||*argument[0] != '7'\
-    //   printf("umask: octal must be between 0 and 7");
-    // else
+    if (arguments[0][0] < '0'||arguments[0][0] > '7')
+       printf("umask: octal must be between 0 and 7\n");
+    else
+      printf("YAY!\n");
     // { 
-    //   i = atoi(argument);
+    //   i = atoi(argument[0][0]);
     //   funix->umask = i;
     //   funix->time = funix->time + 1;
+    //
     // }
+
   }
 }
